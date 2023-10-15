1a6112cc4c7a8a3da1c0e9a5461dee8930a7d509
min_moon <minhee610@gmail.com>
2023-10-15 18:07:44 +0900

    import 수정

--- src/main/java/board/domain/ArticleComment.java
+++ src/main/java/board/domain/ArticleComment.java
@@ -1,10 +1,10 @@
 package board.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
