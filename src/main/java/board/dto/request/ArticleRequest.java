52e0082e39e08efbf4dbaa0dee3588185a614950
min_moon <minhee610@gmail.com>
2023-10-15 17:27:05 +0900

    게시판 뷰 관련 구현

---
+++ src/main/java/board/dto/request/ArticleRequest.java
@@ -0,0 +1,29 @@
+package board.dto.request;
+
+import board.dto.ArticleDto;
+
+import java.util.Set;
+
+public record ArticleRequest(
+        String title,
+        String content
+) {
+
+    public static ArticleRequest of(String title, String content) {
+        return new ArticleRequest(title, content);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return toDto(userAccountDto, null);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto, Set<HashtagDto> hashtagDtos) {
+        return ArticleDto.of(
+                userAccountDto,
+                title,
+                content,
+                hashtagDtos
+        );
+    }
+
+}
